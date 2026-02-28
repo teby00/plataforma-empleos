f10c800712aee3dc481858e7c660020949a219cf
Esteban Daniel Rodríguez Martín <71105277+teby00@users.noreply.github.com>
2026-02-28 02:39:22 -0500

    modificando configuracion de auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/index";
+import { localization } from "better-auth-localization";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -8,11 +9,17 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    minPasswordLength: 6,
   },
   user: {
     additionalFields: {
-      description: { type: "string", required: true },
+      description: { type: "string", required: false },
       role: { type: "string" },
     },
   },
+  plugins: [
+    localization({
+      defaultLocale: "es-ES",
+    }),
+  ],
 });
